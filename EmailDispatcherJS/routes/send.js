8bc0c5e9897604a1009a9fe16ef53bdd8546bdf4
andy <andy@andydote.co.uk>
2014-07-27 13:54:14 +0100

    send.async: parse request body for fields

--- EmailDispatcherJS/routes/send.js
+++ EmailDispatcherJS/routes/send.js
@@ -16,7 +16,19 @@ router.get('/', function(req, res) {
 })
 
 router.post('/async', function(req, res) {
-	res.json({});
+
+	var to = JSON.parse(req.body.to);
+	var from = JSON.parse(req.body.from);
+	var subject = req.body.subject;
+	var body = req.body.body;
+
+	console.log("Email:")
+	console.log("  From: " + from.Name + " (" + from.address + ")");
+	console.log("  To: " + to[0].Name + " (" + to[0].address + ")");
+	console.log("  Subject: " + subject);
+	console.log("  Body: " + body);
+
+	res.json({ status: "success", to: to[0].name, from: from.name});
 });
 
 router.post('/await', function(req, res) {
