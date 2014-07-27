1f0df2dd62e10fe34fbf151d6491d0ed99de72ea
andy <andy@andydote.co.uk>
2014-07-27 13:59:39 +0100

    tests: remove unused models

---
+++ EmailDispatcher.TestApp/Infrastructure/Sender.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mail;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace EmailDispatcher.TestApp.Infrastructure
+{
+	public class Sender
+	{
+		private readonly Uri _host;
+
+		public Sender(Uri host)
+		{
+			_host = host;
+		}
+
+		public void Send(MailMessage mail)
+		{
+			var values = new Dictionary<string, string>();
+			values["to"] = Serialize(mail.To.Select(t => new EmailAddress(t.DisplayName, t.Address)));
+			values["from"] = Serialize(new EmailAddress(mail.From.DisplayName, mail.From.Address));
+			values["subject"] = mail.Subject;
+			values["body"] = mail.Body;
+
+			var content = new MultipartFormDataContent();
+
+			foreach (var value in values)
+			{
+				content.Add(new StringContent(value.Value), value.Key);
+			}
+
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Post,
+				RequestUri = _host,
+				Content = content
+			};
+
+			var client = new HttpClient();
+			var result = client.SendAsync(request).Result;
+
+			Console.WriteLine(result.StatusCode);
+			Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+		}
+
+		private string Serialize(object input)
+		{
+			var settings = new JsonSerializerSettings
+			{
+				ContractResolver =  new CamelCasePropertyNamesContractResolver()
+			};
+
+			return JsonConvert.SerializeObject(input, settings);
+		}
+
+		private struct EmailAddress
+		{
+			public string Name { get; set; }
+			public string Address { get; set; }
+			public Guid ID { get; set; }
+
+			public EmailAddress(string name, string address) : this()
+			{
+				Name = name;
+				Address = address;
+				ID = Guid.Empty;
+			}
+		}
+	}
+}
---
+++ EmailDispatcher.TestApp/SendTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using EmailDispatcher.TestApp.Infrastructure;
+using Xunit;
+
+namespace EmailDispatcher.TestApp
+{
+	public class SendTests
+	{
+		[Fact]
+		public void Sends_successfully()
+		{
+			var sender = new Sender(new Uri("http://localhost:3000/send/async"));
+
+			var message = new MailMessage();
+			message.To.Add(new MailAddress("to.test@example.com", "To Test"));
+			message.From = new MailAddress("from.test@example.com", "From Test");
+			message.Subject = "This is the subject.";
+			message.Body = "This is the body.";
+
+			sender.Send(message);
+		}
+	}
+}
