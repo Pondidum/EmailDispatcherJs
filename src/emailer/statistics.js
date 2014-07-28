5115da8a47e2952af265f0410005dce20361427c
andy <andy@andydote.co.uk>
2014-07-28 20:38:41 +0100

    stats: write a doc to the db

--- src/emailer/statistics.js
+++ src/emailer/statistics.js
@@ -2,7 +2,17 @@ var Datastore = require("nedb");
 var db = new Datastore({ filename: "app_data/stats.db", autoload: true });
 
 
-var logEmail = function() {
+var logEmail = function(email, result) {
+
+	var doc = {
+		email: email,
+		status: result.status,
+		info: result.info
+	};
+
+	db.insert(doc, function(err, newDoc) {
+		//do i care if it failed? i don't know...
+	});
 
 };
 
