dca225bb2f3ff68ae0b1c73aa40947ef31b08fce
andy <andy@andydote.co.uk>
2014-07-29 17:58:33 +0100

    stats: add simple proof of concept stats page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var stats = require('../emailer/stats');
+
 /* GET home page. */
 router.get('/', function(req, res) {
 
   res.render('index', {
   	title: 'EmailDispatcherJS'
   });
+
+});
+
+router.get('/stats', function(req, res) {
+
+	stats.sentFromCount(function(groups) {
+
+		//console.log(groups);
+		res.render('stats', {
+			groups: groups
+		});
+
+	});
 
 });
 
