1c6328c091e697a021bd374d38573c9a83674d37
andy <andy@andydote.co.uk>
2014-07-30 19:58:01 +0100

    statsControls: use list of stats, rather than single functions

--- src/public/js/stats-controls.js
+++ src/public/js/stats-controls.js
@@ -1,15 +1,22 @@
 
+var statsList = [];
 
-var showTotal = function() {
+statsList.push(function() {
+
 	$.ajax('/stats/totalsent', {
 		success: function(json) {
 			$('#total').text(json.count);
 		}
 	});
-};
 
-$(document).ready(function() {
-	showTotal();
 });
 
-//setInterval(, 1000);
+
+
+$(document).ready(function() {
+
+	for(var stat in statsList)
+	{
+		stat();
+	}
+});
