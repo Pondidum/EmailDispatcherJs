91b30e6ba7e1b387f85b77d3448dab542cb4c307
andy <andy@andydote.co.uk>
2014-07-31 17:57:44 +0100

    statsControls: start expanding each control

--- src/public/js/stats-controls.js
+++ src/public/js/stats-controls.js
@@ -1,31 +1,31 @@
 
 var statsList = [];
 
-statsList.push(function() {
-
-	$.ajax('/stats/totalsent', {
-		success: function(json) {
-			$('#total').text(json.count);
-		}
-	});
-
+statsList.push({
+	action: function() {
+		$.ajax('/stats/totalsent', {
+			success: function(json) {
+				$('#total').text(json.count);
+			}
+		});
+	}
 });
 
-statsList.push(function() {
-
-	$.ajax('/stats/lastfive', {
-		success: function(json) {
-			console.log(json);
-			$('#lastfive').text(json[0].from);
-		}
-	})
+statsList.push({
+	action: function() {
+		$.ajax('/stats/lastfive', {
+			success: function(json) {
+				$('#lastfive').text(json[0].from);
+			}
+		});
+	}
 });
 
 
 $(document).ready(function() {
 
 	for (var i = 0; i < statsList.length; i++) {
-		statsList[i]();
+		statsList[i].action();
 	}
 
 });
